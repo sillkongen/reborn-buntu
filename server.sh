309c89cf80ac923cb2c29f5fa6b3ece79d2a2423
Oskar Agustsson <oskar.agustsson@gmail.com>
2021-09-08 13:26:53 +0000

    Update server.sh

--- server.sh
+++ server.sh
@@ -14,7 +14,7 @@ ufw --force enable
 # Samba
 ufw allow proto udp to any port 137 from 192.168.1.0/24
 ufw allow proto udp to any port 138 from 192.168.1.0/24
-ufw allow proto tcp to any port 139 from 192.168.1.0/24
+ufw allow proto tcp to any port 139 from 192.168.1.0/24flexfw
 ufw allow proto tcp to any port 445 from 192.168.1.0/24
 
 # Add mounter for merged volumes.
@@ -23,9 +23,15 @@ ufw allow proto tcp to any port 445 from 192.168.1.0/24
 # Nebula
 # Add certificates - startup process - and configuration
 
+# Static IP configuration (Network Manager for Ubuntu desktop and networkd for server)
 cp 01-netcfg.yaml /etc/netplan/01-network-manager-all.yaml
-
 netplan apply
 
-shutdown now -r
+#Plex Firewall rules
+cp flexfw /etc/ufw/applications.d/plexmediaserver
+ufw app update plexmediaserver
+ufw allow plexmediaserver-all
+
+echo "Verify your configuration, if you feel lucky then REBOOT!"
+# shutdown now -r
 
